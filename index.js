1804a173af0e1ebc8ef821c168944153c7426f14
GWilman <georgewilman1@gmail.com>
2018-11-17 22:39:01 +0000

    initial commit - set up nodemailer

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 3000;
+const emailConfig = require('./email_config.js');
+
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: emailConfig.EMAIL_ADDRESS,
+    pass: emailConfig.EMAIL_PASSWORD
+  }
+});
+
+app.use(bodyParser.json());
+
+app.post('/email', (req, res) => {
+
+  const mailOptions = {
+    from: emailConfig.EMAIL_ADDRESS, // sender address
+    to: req.body.sendTo, // list of receivers
+    subject: 'New message for Lawful', // Subject line
+    html: `<p>${req.body.message}</p><p>${req.body.fromName} - ${req.body.fromEmail}</p>`// plain text body
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) {
+      console.log(err);
+      res.send('email error');
+    } else {
+      console.log(info);
+      res.send('email sent');
+    }
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
